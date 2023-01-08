dfffdc487d68417ca41ee5eb743a6fe604a2e662
Abdessamad <80596901+Abdessamad-Y@users.noreply.github.com>
2023-01-08 20:07:58 +0000

    modified:   src/App.js 	new file:   src/components/Header.jsx 	modified:   src/scenes/Team/index.jsx

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
           <Topbar/>
           <Routes>
             <Route path='/' element={<Dashboard />} />
-            <Route path='/team' element={<Team />}
+            <Route path='/team' element={<Team />} />
           </Routes>
           
           </main>
