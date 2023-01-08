b15ad4e2deba30b163871525f5e0d6da9983c3c2
Abdessamad <80596901+Abdessamad-Y@users.noreply.github.com>
2023-01-08 10:53:22 +0000

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/scenes/global/Sidebar.jsx 	modified:   src/theme.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import {ColorModeContext, colorModeContext,useMode} from './theme'
+import {Routes,Route} from 'react-router-dom'
 import {CssBaseline,ThemeProvider} from '@mui/material'
 import Topbar from './scenes/global/Topbar'
-import Sidebar from './scenes/global/Sidebar'
-import {Routes,Route} from 'react-router-dom'
+import SideBar from './scenes/global/Sidebar'
+import Dashboard from "./scenes/dashboard";
+//import Team from "./scenes/team";
+//import Invoices from "./scenes/invoices";
+//import Contacts from "./scenes/contacts";
+//import Bar from "./scenes/bar";
+//import Form from "./scenes/form";
+//import Line from "./scenes/line";
+//import Pie from "./scenes/pie";
+//import FAQ from "./scenes/faq";
+//import Geography from "./scenes/geography";
+//import Calendar from "./scenes/calendar/calendar";
 
+import { useState } from "react";
 function App() {
   const [theme,colorMode] = useMode()
+  const [isSidebar, setIsSidebar] = useState(true);
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Sidebar />
+        
         <div className="app">
+          <SideBar />
           <main className='content'>
-            <Topbar />
-            <Routes>
-
-            </Routes>
+          <Topbar/>
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+          </Routes>
+          
           </main>
         </div>
       </ThemeProvider>
